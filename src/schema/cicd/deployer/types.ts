839eb09c8736fadfaa8b2c4ff1ab02ef9aa70b37
Michael Munson <michael.s.munson@outlook.com>
2025-02-20 11:51:05 -0500

    push

--- src/schema/cicd/deployer/types.ts
+++ src/schema/cicd/deployer/types.ts
@@ -1,4 +1,4 @@
-import { Field, SchemaInput } from "../../types";
+import { CustomField, Field, SchemaInput, Table } from "../../types";
 import { Client } from "pg";
 
 export type SchemaDeployerConfig = {
@@ -6,19 +6,23 @@ export type SchemaDeployerConfig = {
   client?: ConstructorParameters<typeof Client>[0];
 }
 
-export type SchemaChange = {changeType: 'CREATE' | 'ALTER' | 'DROP';} & ({
-  type: 'TABLE';
-  name: string;
-  fields: Record<string, Field<any, any>>;
-} | {
-  type: 'FIELD';
-  tableName: string;
-  name: string;
-  field: Field<any, any>;
-})
+// export type SchemaChange = {changeType: 'CREATE' | 'ALTER' | 'DROP';} & ({
+//   type: 'TABLE';
+//   name: string;
+//   fields: Record<string, Field<any, any>>;
+// } | {
+//   type: 'FIELD';
+//   tableName: string;
+//   name: string;
+//   field: Field<any, any>;
+// })
 
-export type SchemaChangeSet = {
-  create: SchemaInput;
-  alter: SchemaInput;
-  drop: SchemaInput;
-}
+type AnyField = Field<any,any> | CustomField<any>
+
+export type SchemaChange = (
+  {type: 'CREATE_TABLE', name: string, fields:Table} | 
+  {type: 'DROP_TABLE', name:string} | 
+  {type: 'ADD_COLUMN', tableName: string, name:string, field:AnyField} |
+  {type: 'ALTER_COLUMN', tableName:string, name:string, field:AnyField} |
+  {type: 'DROP_COLUMN', tableName:string, name:string}
+)
--- src/schema/cicd/deployer/utils.ts
+++ src/schema/cicd/deployer/utils.ts
@@ -1,31 +1,60 @@
 import { FieldSchema } from "../../../client/types";
-import { PostgresSchemaMap, PostgresTableMap, SchemaInput, SchemaMap } from "../../types";
-import { SchemaChangeSet, SchemaDeployerConfig } from "./types";
+import { Field, PostgresSchemaMap, PostgresTableMap, SchemaInput, SchemaMap } from "../../types";
+import { SchemaChange, SchemaDeployerConfig } from "./types";
 
 export const DEFAULT_CONFIG: SchemaDeployerConfig = {
   autoDeploy: false,
 }
 
-export const compareSchema = (newSchema: SchemaInput, currentSchema: PostgresSchemaMap) : SchemaChangeSet => {
-  const changeSet:SchemaChangeSet = {
-    create: {},
-    alter: {},
-    drop: {},
-  }
+export const compareSchema = (newSchema: SchemaInput, currentSchema: PostgresSchemaMap) : SchemaChange[] => {
+  const changeSet:SchemaChange[] = [];
   // CREATE
   for (const table in newSchema) {
     if (!currentSchema.has(table)) {
-      changeSet.create[table] = newSchema[table];
+      changeSet.push({
+        type: 'CREATE_TABLE',
+        name: table,
+        fields: newSchema[table],
+      });
+    }
+    else {
+      const currentTable = currentSchema.get(table)!;
+      const newTable = newSchema[table]!;
+      for (const [field] of currentTable.entries()) {
+        if (!(field in newTable)) {
+          changeSet.push({
+            type: 'DROP_COLUMN',
+            tableName: table,
+            name: field
+          });
+        }
+      }
+      for (const field in newTable){
+        if (!currentTable.has(field)){
+          changeSet.push({
+            type: 'ADD_COLUMN',
+            tableName: table,
+            name: field,
+            field: newTable[field]
+          })
+        } else {
+          changeSet.push({
+            type: 'ALTER_COLUMN',
+            tableName: table,
+            name: field,
+            field: newTable[field]
+          })
+        }
+      }
     }
   }
-  // ALTER
-  for (const table in newSchema) {
-    const alterChanges:SchemaChangeSet['alter'] = {};
-    const currentTable = currentSchema.get(table);
-    if (currentTable) {
-      for (const [field, fieldSchema] of currentTable.entries()) {
-        
-      }
+  // DROP
+  for (const table in currentSchema) {
+    if (!(table in newSchema)) {
+      changeSet.push({
+        type: 'DROP_TABLE',
+        name: table
+      });
     }
   }
   return changeSet;
